5f783386ac69ffe7b2557752f6c36ca4fca0775b
nabousoum <seynabousoumare98@outlook.fr>
2022-03-25 15:59:29 +0000

    us3 sprint 1

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-alert('a');
+const header = document.querySelector('.header');
+const burger = document.getElementById('menu-burger');
+const container = document.getElementById('container');
+const colonne = document.getElementById('colonne');
+
+burger.addEventListener('click',function(){
+    header.classList.toggle('closeMenu');
+})
+
+colonne.addEventListener('click',function(){
+    // var i=0;
+    // while(i>5){
+        createColumn();
+    // }
+    // i++;
+})
+
+function createColumn(){
+    const divG = document.createElement('div');
+    divG.className="column";
+
+    const div1 = document.createElement('div');
+    div1.className="column-name";
+    const p = document.createElement('p');
+    p.innerText="colonne 1";
+
+    const div2 = document.createElement('div');
+    div2.className="column-contain";
+
+    const divLogo = document.createElement('div');
+    divLogo.className="logo";
+
+    const img = document.createElement('img');
+    img.setAttribute('height','120px');
+    img.src="logoMyCutiePlanning.png";
+
+    divLogo.appendChild(img);
+    div1.appendChild(p);
+    divG.appendChild(div1);
+    divG.appendChild(div2);
+    divG.appendChild(divLogo);
+    container.appendChild(divG);
+}
